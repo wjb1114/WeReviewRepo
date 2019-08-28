4803c2b5de4a9bb3db90a919e0f59eebc1feb753
wjb1114 <kbd1114@gmail.com>
2019-08-28 09:48:02 -0500

    Implemented GitHub authentication with OAuth and display of signed in user's repositories.

--- WeReview/WeReview/Startup.cs
+++ WeReview/WeReview/Startup.cs
@@ -12,6 +12,13 @@ using Microsoft.EntityFrameworkCore;
 using WeReview.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
 
 namespace WeReview
 {
@@ -42,6 +49,49 @@ namespace WeReview
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = "GitHub";
+            })
+        .AddCookie()
+        .AddOAuth("GitHub", options =>
+        {
+            options.ClientId = Configuration["GitHub:ClientId"];
+            options.ClientSecret = Configuration["GitHub:ClientSecret"];
+            options.CallbackPath = new PathString("/signin-github");
+
+            options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+            options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+            options.UserInformationEndpoint = "https://api.github.com/user";
+
+            options.SaveTokens = true;
+
+            options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+            options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+            options.ClaimActions.MapJsonKey("urn:github:login", "login");
+            options.ClaimActions.MapJsonKey("urn:github:url", "html_url");
+            options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
+
+            options.Events = new OAuthEvents
+            {
+                OnCreatingTicket = async context =>
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                    request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+                    var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                    response.EnsureSuccessStatusCode();
+
+                    var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+                    context.RunClaimActions(user);
+                }
+            };
+        });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,7 +114,12 @@ namespace WeReview
 
             app.UseAuthentication();
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
         }
     }
 }
